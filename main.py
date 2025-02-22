2382094101016fdb58c9d51cc09c5a8782400416
CLiv <livchris89@gmail.com>
2025-02-21 21:31:24 -0800

    2: saving files

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import *
+from asteroidfield import *
+import sys
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
-    # try:
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
+
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over")
+                sys.exit()
+
+        #Draws the screen
+        screen.fill("black")
+        
+        #Draws the player
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
-    # except KeyboardInterrupt:
-    #     print("Exiting")
-    
-    
+        
+        #Limit game to run 60fps
+        dt = clock.tick(60) / 1000
+
+
+
+
 
 if __name__ == "__main__":
     main()
